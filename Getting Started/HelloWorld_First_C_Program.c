6daf3bf2c86f871fb8ae7fe19f19d5d192258cbb
Shubham Kushwah <shubhamkushwahsk@gmail.com>
2017-02-06 23:05:23 +0530

    Create HelloWorld_First_C_Program.c

---
+++ Getting Started/HelloWorld_First_C_Program.c
@@ -0,0 +1,6 @@
+#include <stdio.h>  //header file used for using various libraries such as input/output functions
+
+int main(){     //the main function is the head of every program, each and every program starts executing from the main() function
+    printf("Hello, World!");    //this is the function in which --> printf(argument); here argument is that value what we want to print out.
+    return 0;   //since the main function is of type 'int', therefore it must return a value,but return 0; here '0' determines that the program ran successfully.
+}
